e6712a08b7cbaad5083815e472010ebdc330f831
Ahmed <ahmed.alidjinou@hotmail.com>
2020-10-05 14:04:11 +0200

    add of function

--- src/pierreFeiulleCiseaux/play.java
+++ src/pierreFeiulleCiseaux/play.java
@@ -8,6 +8,65 @@ public class play {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		play2();
+	}
+
+	public static void play2() {
+		String[] gameTable = { "Pierre", "Feuille", "Ciseau" };
+		boolean play = true;
+		int gameLoose = 0, gameWin = 0;
+		int propositionJeux;
+		String rejouer;
+
+		do {
+			System.out.println("Bienvenue dans le jeux !\n");
+			System.out.println("List des propositions");
+			System.out.println("1: Pierre");
+			System.out.println("2: Feuille");
+			System.out.println("3: Ciseau\n");
+			Random random = new Random();
+			int nb = random.nextInt(3 - 1 + 1) + 1;
+			boolean propoRight = true;
+			String result = "Vous avez ";
+			do {
+				propoRight = true;
+				System.out.println("Entrez votre proposition");
+				propositionJeux = Clavier.lireInt();
+				if (propositionJeux < 1 || propositionJeux > 3) {
+					System.out.println("Veuillez entrez une des proposition");
+					propoRight = false;
+				}
+
+			} while (propoRight == false);
+
+			System.out.println("Vous avez choisi " + gameTable[propositionJeux - 1] + "  et le choix de l'ordi est  "
+					+ gameTable[nb - 1]);
+			if (propositionJeux == nb) {
+				result = "Egalité !\n"; 
+			} else if ((propositionJeux == 1 && nb == 3) || (propositionJeux == 2 && nb == 1)
+					|| (propositionJeux == 3 && nb == 2)) {
+				result += "Gagné !\n";
+				gameWin++;
+			} else {
+				result += "Perdu !\n";
+				gameLoose++;
+			}
+
+			System.out.println(result);
+			System.out.println("Voulez vous rejouer ? O/N");
+			rejouer = Clavier.lireString();
+			if (rejouer.equals("N")) {
+				play = false;
+			}
+
+		} while (play);
+
+		System.out.println(gameWin + " Partie gagné");
+		System.out.println(gameLoose + " Partie perdu");
+	}
+
+	public static void playe() {
 		String[] gameTable = { "pierre", "feuille", "ciseau" };
 		int play = 0;
 		int gameLoose = 0, gameWin = 0;
@@ -90,7 +149,7 @@ public class play {
 					System.out.println("Veuillez entrez une des proposition");
 					propoRight = false;
 				}
-			} while (propoRight==false);
+			} while (propoRight == false);
 
 			System.out.println("Voulez vous rejouer ? O/N");
 			rejouer = Clavier.lireString();
